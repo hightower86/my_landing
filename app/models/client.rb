dc94eb426b65d462a34a54a2b003cb0778fb8ee2
Adel <htwr86@gmail.com>
2018-07-11 10:55:42 +0300

    add lastname and validations+phone

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,7 @@ class Client < ApplicationRecord
   validates :name,  :last_name, presence: true, length: { minimum: 2}
   validates :email, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :phone,:presence => true,
+            :numericality => true,
+            :length => { :minimum => 10, :maximum => 15 }
 end
